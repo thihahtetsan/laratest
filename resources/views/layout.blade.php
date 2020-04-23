0588731c2f37edf81f526d67d8d69d428378f970
Thiha-Htetsan <formerchild@users.noreply.github.com>
2020-04-23 07:30:44 +0630

    basic

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Laravel 5.8 CRUD Example Tutorial</title>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.0/css/bootstrap.css" rel="stylesheet" />
+</head>
+<body>
+  <div class="container">
+    @yield('content')
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.0/js/bootstrap.min.js"></script>
+</body>
+</html>
